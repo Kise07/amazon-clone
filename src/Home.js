bffa8c89c36dd985ad4d894af0e5eb3ec93a6767
kise <kaiazmi1412@gmail.com>
2021-09-24 22:54:28 +0530

    Added Product Components

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home--container">
+        <img
+          className="home--image"
+          src="https://m.media-amazon.com/images/I/71UhgZQzskL._SX3000_.jpg"
+          alt=""
+        />
+
+        <div className="home--row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home--row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home--row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
