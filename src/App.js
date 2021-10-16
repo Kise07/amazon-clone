feebc394432c3ab0c8c16d65a7ffe68539e40700
kise <kaiazmi1412@gmail.com>
2021-10-16 18:40:49 +0530

    Added Checkout Cart Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
-import "./App.css";
-import Headers from "./Header";
-import Home from "./Home";
+import React from 'react';
+import './App.css';
+import Headers from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Headers />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Headers />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Headers />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
